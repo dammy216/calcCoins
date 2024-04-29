bae102f5543ae6c6c4dd72ed7d8fb8b13f88598a
dammy216 <dammy3612@icloud.com>
2024-04-29 20:00:54 +0900

    プロジェクト ファイルを追加します。

---
+++ Training2/CoinCase.cs
@@ -0,0 +1,76 @@
+﻿namespace Training2
+{
+    internal class CoinCase
+    {
+        private int _500yen;
+        private int _100yen;
+        private int _50yen;
+        private int _10yen;
+        private int _5yen;
+        private int _1yen;
+
+        public void AddCoins(int kinds, int amount)
+        {
+            if (kinds == 500)
+                _500yen += amount;
+            if (kinds == 100)
+                _100yen += amount;
+            if (kinds == 50)
+                _50yen += amount;
+            if (kinds == 10)
+                _10yen += amount;
+            if (kinds == 5)
+                _5yen += amount;
+            if (kinds == 1)
+                _1yen += amount;
+        }
+
+        public int GetCount(int kinds)
+        {
+            if (kinds == 500)
+                return _500yen;
+            if (kinds == 100)
+                return _100yen;
+            if (kinds == 50)
+                return _50yen;
+            if (kinds == 10)
+                return _10yen;
+            if (kinds == 5)
+                return _5yen;
+            if (kinds == 1)
+                return _1yen;
+
+            return 0;
+        }
+
+        public int GetCount()
+        {
+            return _500yen + _100yen + _50yen + _10yen + _5yen + _1yen;
+        }
+
+        public int GetAmount()
+        {
+            var answer = 0;
+            return answer = 500 * _500yen + 100 * _100yen + 50 * _50yen + 10 * _10yen + 5 * _5yen + 1 * _1yen;
+        }
+
+        public int GetAmount(int kinds)
+        {
+            if (kinds == 500)
+                return 500 * _500yen;
+            if (kinds == 100)
+                return 100 * _100yen;
+            if (kinds == 50)
+                return 50 * _50yen;
+            if (kinds == 10)
+                return 10 * _10yen;
+            if (kinds == 5)
+                return 5 * _5yen;
+            if (kinds == 1)
+                return 1 * _1yen;
+
+            return 0;
+        }
+    }
+
+}
---
+++ Training2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Training2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var coinCase = new CoinCase();
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("コインの種類");
+                int kinds = int.Parse(Console.ReadLine());
+                Console.WriteLine("コインの枚数");
+                int coinAmount = int.Parse(Console.ReadLine());
+
+                coinCase.AddCoins(kinds, coinAmount);
+            }
+
+            Console.WriteLine($"500円は{coinCase.GetCount(500)}枚、総額{coinCase.GetAmount(500)}円");
+            Console.WriteLine($"100円は{coinCase.GetCount(100)}枚、総額{coinCase.GetAmount(100)}円");
+            Console.WriteLine($"50円は{coinCase.GetCount(50)}枚、総額{coinCase.GetAmount(50)} 円");
+            Console.WriteLine($"10円は{coinCase.GetCount(10)}枚、総額{coinCase.GetAmount(10)} 円");
+            Console.WriteLine($"5円は{coinCase.GetCount(5)}枚、総額{coinCase.GetAmount(5)}  円");
+            Console.WriteLine($"1円は{coinCase.GetCount(1)}枚、総額{coinCase.GetAmount(1)} 円");
+            Console.WriteLine($"合計は{coinCase.GetAmount()}円");
+            Console.WriteLine($"硬貨の総数は{coinCase.GetCount()}");
+
+        }
+    }
+}
